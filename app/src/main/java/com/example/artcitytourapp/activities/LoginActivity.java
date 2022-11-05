e16ab94b12db291f575fc82fdb37b6a39ba63ef6
Kaled Sánchez <kaledsv@gmail.com>
2022-11-05 02:55:22 -0600

    Actividades de login

---
+++ app/src/main/java/com/example/artcitytourapp/activities/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.artcitytourapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;import com.example.artcitytourapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
