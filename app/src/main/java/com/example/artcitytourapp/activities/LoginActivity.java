0d00a223de75bb80a5e1198be337e87c3f16608e
Kaled Sánchez <kaledsv@gmail.com>
2022-11-05 23:34:48 -0600

    Login y sign in con google

--- app/src/main/java/com/example/artcitytourapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/artcitytourapp/activities/LoginActivity.java
@@ -1,9 +1,14 @@
 package com.example.artcitytourapp.activities;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -16,10 +21,22 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.artcitytourapp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import java.util.Objects;
 
 import Usuario.VisitanteSingleton;
 
@@ -30,6 +47,9 @@ public class LoginActivity extends AppCompatActivity {
     ProgressBar progressBar;
 
     FirebaseAuth fAuth;
+    private static final int RC_SIGN_IN = 100;
+    private GoogleSignInClient googleSignInClient;
+    ActivityResultLauncher<Intent> startActivityForResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +57,30 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         loaddata();
+        startActivityForResult = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        Log.d("Codigo", String.valueOf(result.getResultCode()));
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            Log.d("Existo", "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+                            Intent data = result.getData();
+                            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(data);
+                            try{
+                                GoogleSignInAccount account = accountTask.getResult(ApiException.class);
+                                firebaseAuthWithGoogle(account);
+                            }catch (Exception e){
+                                new AlertDialog.Builder(getApplicationContext())
+                                        .setTitle("Error")
+                                        //.setMessage("" + e.getMessage())
+                                        .setMessage("Google no pudo verificar sus credenciales")
+                                        .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> dialogInterface.dismiss())
+                                        .show();
+                            }
+                        }
+                    }
+                });
     }
 
     void loaddata(){
@@ -79,32 +123,75 @@ public class LoginActivity extends AppCompatActivity {
                         // change to main
                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
                     }else{
-                        new AlertDialog.Builder(view.getContext())
+                        new AlertDialog.Builder(getApplicationContext())
                                 .setTitle("Error")
                                 //.setMessage("" + task.getException())
                                 .setMessage("El usuario no se ha podido verificar, revise los datos")
-                                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                        dialogInterface.dismiss();
-                                    }
-                                })
+                                .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> dialogInterface.dismiss())
                                 .show();
                     }
                 }
             });
         });
+
         loginGoogle.setOnClickListener(view -> {
-            // todo
+            loginGoogle();
         });
+
         loginFacebook.setOnClickListener(view -> {
             // todo
         });
+
         createAccountTextView.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
         });
         forgotPassTextView.setOnClickListener(view -> {
             startActivity(new Intent(getApplicationContext(), RecuperatePassActivity.class));
+        });
+    }
+
+    private void loginGoogle(){
+        GoogleSignInOptions googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+
+        Intent intent = googleSignInClient.getSignInIntent();
+        startActivityForResult.launch(intent);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account){
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        fAuth.signInWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+            @Override
+            public void onSuccess(AuthResult authResult) {
+                String uid = Objects.requireNonNull(fAuth.getCurrentUser()).getUid();
+                String correo = Objects.requireNonNull(fAuth.getCurrentUser()).getEmail();
+
+                Log.d("DATOSSSSS", "uid: " + uid + ", correo: " + correo);
+                Log.d("DATOSSSSS", String.valueOf(Objects.requireNonNull(authResult.getAdditionalUserInfo()).isNewUser()));
+                if (Objects.requireNonNull(authResult.getAdditionalUserInfo()).isNewUser()){
+                    //register
+                    VisitanteSingleton.CreateVisitante(uid, correo);
+                }else{
+                    //login
+                    Log.d("user", uid);
+                    VisitanteSingleton.LoginVisitante(uid);
+                }
+                // change to main
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                new AlertDialog.Builder(getApplicationContext())
+                        .setTitle("Error")
+                        //.setMessage("" + task.getException())
+                        .setMessage("El usuario no se ha podido verificar, revise que ya este registrado")
+                        .setPositiveButton(android.R.string.ok, (dialogInterface, i) -> dialogInterface.dismiss())
+                        .show();
+            }
         });
     }
 }
